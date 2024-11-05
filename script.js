97e9b5d9a993e8bfcce5c8bd753794f24b844056
gega109 <bakuravar1@gmail.com>
2024-11-06 01:03:07 +0400

    homework 21

---
+++ script.js
@@ -0,0 +1,128 @@
+// for (let index = 0; index < 15; index++) {
+//   if (index >= 5) {
+//     console.log(index, "es ricxvi metia xutze");
+//   } else {
+//     console.log(index);
+//   }
+// }
+
+// let numbers = [3, 6, 9, 12, 15];
+
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] % 2 === 0) {
+//     console.log(numbers[index]);
+
+//   } else {
+
+//   }
+// }
+
+// let str = "me var nikis moswavle"
+// for (let index = 0; index < str.length; index++) {
+//     if (str[index] === 'a') {
+//         console.log(str[index] + "- a aso");
+//     } else {
+//         console.log(str[index]);
+
+//     }
+
+// }
+// let words = ["red", "green", "blue", "yellow"];
+// let longWords = words.filter((word) => word.length > 5);
+// console.log(longWords);
+
+// for (let index = 1; index < 50; index++) {
+//   if (index % 7) {
+//   } else {
+//     console.log(index);
+// გუგლ მეთოდ დავსერჩე და ისე დავწერე დდ
+
+//   }
+// }
+// let numbers = [4, 8, 12, 16, 20];
+// for (let index = 0; index < numbers.length; index++) {
+//   console.log(numbers[index] * 2);
+// }
+
+// let word = "javascript";
+// გუგლ მეთოდ დავსერჩე და ისე დავწერე დდ
+
+// for (let i = 0; i < word.length; i++) {
+//   if (word[i] === "j") {
+//     console.log("");
+//   } else {
+//     console.log(word[i]);
+//   }
+// }
+
+// let numbers = [5, 10, 15, 20, 25];
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] % 5) {
+//     } else {
+//         console.log(numbers[index]);
+
+//     }
+
+// }
+// let words = "hello world";
+// for (let i = 0; i < words.length; i++) {
+//   if (words[i] !== "o") {
+//     console.log(words[i]);
+//   }
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let SumOfEvens = numbers.reduce((sum, number) => {
+//   if (number % 2 === 0) {
+//     return sum + number;
+//   }
+//   return sum;
+// }, 0);
+// console.log(SumOfEvens);
+// გუგლ მეთოდ დავსერჩე და ისე დავწერე დდ
+
+// for (let index = 1; index <= 10; index++) {
+//   console.log(`10 x ${index} = ${10 * index}`);
+// }
+// გუგლ მეთოდ დავსერჩე და ისე დავწერე დდ
+
+// let numbers = [2, 7, 4, 9, 5];
+// let result = [];
+
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] > 4) {
+//     result.push(numbers[index] * 3);
+//   } else {
+//     result.push(numbers[index]);
+//   }
+// }
+
+// console.log(result);
+// გუგლ მეთოდ დავსერჩე და ისე დავწერე დდ
+
+// for (let index = 1; index <= 30; index++) {
+//   if (index > 10) {
+//     console.log(index + " ეს რიცხვი 10ზე მეტია");
+//   } else {
+//     console.log(index);
+//   }
+// }
+
+// let str = "frontend"
+// for (let index = 0; index < str.length; index++) {
+//     if (str[index] === "e") {
+//         console.log("ნაპოვნია ასო " + str[index]);
+//     } else {
+//         console.log(str[index]);
+
+//     }
+
+// }
+// let numbers = [100, 200, 300, 400, 500];
+// for (let index = 0; index < numbers.length; index++) {
+//   if (numbers[index] > 250) {
+//     console.log(numbers[index]);
+//   }
+// }
+
+// რასაც მივაწერე მხოლოდ ისინი ვნახე გუგლში
